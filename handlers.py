aef6fa2e442a3362ce59f7423f8bab0e9d35e997
niik <fk724s@mail.ru>
2023-12-19 11:37:45 +0300

    fill in some handlers

--- handlers.py
+++ handlers.py
@@ -1,22 +1,26 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from filters import TextFilter
 
 router = Router()
 
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer("ksrhka")
+    await msg.answer("Hello, world!")
+
+
+@router.message(F.text == 'Test')
+async def filtered_message_handler(msg: Message):
+    await msg.answer("Test")
+
+
+@router.message(F.content_type.in_({'photo'}))
+async def photo_handler(msg: Message):
+    await msg.answer("Thanks for your photo!")
 
 
 @router.message()
 async def message_handler(msg: Message):
-    await msg.answer("")
-
-
-@router.message(TextFilter("Задачи"))
-async def filtered_message_handler(msg: Message):
-    await msg.answer("")
+    await msg.answer("By this time i can't answer for this type of messages")
